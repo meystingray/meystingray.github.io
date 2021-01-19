d9af43b68d658e299ad7edf366f287323beab03b
Sean Conroy <sconroy@coveypark.com>
2021-01-19 12:26:10 -0600

    About

--- ShinyMurderMap/app.R
+++ ShinyMurderMap/app.R
@@ -19,37 +19,8 @@ token <- readRDS("droptoken.rds")
 drop_download(path = "Shiny/Murder.RDS",local_path =  paste0(tempdir(), "/MurderData.RDS"),overwrite = TRUE, dtoken = token)
 Murder <- readRDS(paste0(tempdir(), "/MurderData.RDS"))
 
-#Murder <- readRDS("C:/Users/sconroy/Documents/DallasPoliceData/Murder.RDS")
-#Murder <- drop_read_rds("Shiny/Murder.RDS",dtoken = token)
 
-#setwd("C:/Users/sconroy/Documents/meystingray.github.io")
-
-# 
-# Murder[,rowid := 1:.N]
-# Murder[,Date := as.Date(substr(date1,1,10))]
-# Murder[,MonthDate := as.Date(paste0(format(Date,"%Y-%m"),"-01"))]
-# Murder[,WeekNum := strftime(Date, format = "%V")]
-# Murder <- merge(Murder,Murder[,head(.SD, 1L),.SDcols = "Date",by = c("servyr","WeekNum")],by = c("servyr","WeekNum"))
-# setnames(Murder,old = c("Date.x","Date.y"),new = c("Date","WeekDate"))
-# 
-# Murder[,LatLongStart := regexpr("(",geocoded_column,fixed = TRUE)[1] + 1,by = rowid]
-# Murder[,LatLongEnd := regexpr(")",geocoded_column,fixed = TRUE)[1] - 1,by = rowid]
-# Murder[,LatLong := substr(geocoded_column,start = LatLongStart,stop = LatLongEnd)]
-# Murder[,LatLongComma := regexpr(",",LatLong,fixed = TRUE)[1],by = rowid]
-# Murder[,Latitude := substr(LatLong,start = 1,stop = LatLongComma - 1)]
-# Murder[,Longitude := substr(LatLong,start = LatLongComma + 1,stop = 1000)]
-# Murder[,Longitude := as.numeric(Longitude)]
-# Murder[,Latitude := as.numeric(Latitude)]
-# Murder[,Year := as.factor(servyr)]
-# Murder$servyr <- NULL
-# Murder <- Murder[!is.na(Latitude) & !is.na(Longitude) & !is.na(Date) & !is.na(Year),]
-# 
-# Murder[,c("LatLongStart","LatLongEnd","LatLong","LatLongComma") := NULL]
-# 
-# setnames(Murder,old = c("watch","offincident","comprace","compsex","compage","compethnicity","status","victimcond"),
-#          c("Watch","Officer_Incident","Comp_Race","Comp_Sex","Comp_Age","Comp_Ethnicity","Status","Victim_Condition"))
-
-columnChoices <- c("Year","Watch", "Officer_Incident", "Comp_Race", "Comp_Ethnicity", 
+columnChoices <- c("Year","Watch", "Beat", "Officer_Incident", "Comp_Race", "Comp_Ethnicity", 
        "Comp_Sex", "Status","Victim_Condition")
 
 LegendPal <- colorFactor(
@@ -59,10 +30,52 @@ LegendPal <- colorFactor(
     domain = NULL
 )
 
+MurderRefresh <- function(Murder) {
+    
+    lastRefreshDate <- max(Murder$Date,na.rm = TRUE)
+    #print(lastRefreshDate)
+    refreshString <- paste0("https://www.dallasopendata.com/Public-Safety/Police-Incidents/qv6i-rri7?$where=date1>'",
+                            lastRefreshDate,"'")
+    
+    PI <- read.socrata(refreshString)
+    setDT(PI)
+    NewMurder <- PI[grepl("Murder",offincident) | grepl("HOMICIDE",offincident) | grepl("Murder",ucr_offense) | 
+                        grepl("HOMICIDE",nibrs_crime_category) | grepl("Murder",nibrs_crime),]
+    
+    if (nrow(NewMurder) > 0) {
+        NewMurder[,rowid := 1:.N]
+        NewMurder[,Date := as.Date(substr(date1,1,10))]
+        NewMurder[,MonthDate := as.Date(paste0(format(Date,"%Y-%m"),"-01"))]
+        NewMurder[,WeekNum := strftime(Date, format = "%V")]
+        NewMurder <- merge(NewMurder,NewMurder[,head(.SD, 1L),.SDcols = "Date",by = c("servyr","WeekNum")],by = c("servyr","WeekNum"))
+        setnames(NewMurder,old = c("Date.x","Date.y"),new = c("Date","WeekDate"))
+        
+        NewMurder[,LatLongStart := regexpr("(",geocoded_column,fixed = TRUE)[1] + 1,by = rowid]
+        NewMurder[,LatLongEnd := regexpr(")",geocoded_column,fixed = TRUE)[1] - 1,by = rowid]
+        NewMurder[,LatLong := substr(geocoded_column,start = LatLongStart,stop = LatLongEnd)]
+        NewMurder[,LatLongComma := regexpr(",",LatLong,fixed = TRUE)[1],by = rowid]
+        NewMurder[,Latitude := substr(LatLong,start = 1,stop = LatLongComma - 1)]
+        NewMurder[,Longitude := substr(LatLong,start = LatLongComma + 1,stop = 1000)]
+        NewMurder[,Longitude := as.numeric(Longitude)]
+        NewMurder[,Latitude := as.numeric(Latitude)]
+        NewMurder[,Year := as.factor(servyr)]
+        NewMurder$servyr <- NULL
+        NewMurder <- NewMurder[!is.na(Latitude) & !is.na(Longitude) & !is.na(Date) & !is.na(Year),]
+        
+        NewMurder[,c("LatLongStart","LatLongEnd","LatLong","LatLongComma") := NULL]
+        
+        setnames(NewMurder,old = c("beat","watch","offincident","comprace","compsex","compage","compethnicity","status","victimcond"),
+                 c("Beat","Watch","Officer_Incident","Comp_Race","Comp_Sex","Comp_Age","Comp_Ethnicity","Status","Victim_Condition"))
+        Murder <- rbindlist(list(Murder,NewMurder),use.names = TRUE,fill = TRUE)
+    }
+    #print(max(Murder$Date,na.rm = TRUE))
+    Murder
+}
 
 ui <- fluidPage(
     fluidRow(
-        column(12,titlePanel("Exploring Dallas Murders Since 2014"))
+        column(12,titlePanel(p("Exploring Dallas Murders.  An R-Shiny app by ",
+                               a("Sean Conroy",href ='https://www.seantconroy.com/'))))
     ),
     fluidRow(
         column(12,uiOutput("BottomHeader"))
@@ -74,7 +87,7 @@ ui <- fluidPage(
             uiOutput("FILTER_VALUE"),
             selectInput("COLOR_COLUMN", "Color By:", choices = columnChoices,selected = "Year"),
             sliderInput('MarkerSize', 'Marker Size', min = 1, max = 10,value = 5),
-            sliderInput('FillOpacity', 'Fill Opacity', min = 0, max = 1,value = 1)
+            sliderInput('NumClusters', '# Clusters \n (reduce Marker Size to view)', min = 0, max = 30,value = 5)
         ),
         column(6,leafletOutput("LeafletMap")),
         column(2,tableOutput("SummaryTable"))
@@ -90,16 +103,20 @@ ui <- fluidPage(
         )
 )
 
-#shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)
 
 # Define server logic required to draw a histogram
 url <- a("'Police Incidents'",
          href="https://www.dallasopendata.com/Public-Safety/Police-Incidents/qv6i-rri7")
 
 server <- function(input, output) {
-        
+    
+    MurderData <- reactiveValues()
+    MurderData$Murder <- Murder
+    
     output$BottomHeader <- renderUI({
-        tagList("Based on Dallas Open Data source:", url, ". This is not a complete data set; it appears that a significant # of incidents are missing.")
+        tagList("Based on Dallas Open Data source:", url,
+                ". Note: this is not a complete data set; it appears that a significant # of incidents are missing.")
     })
     
     output$FILTER_VALUE <- renderUI({
@@ -126,6 +143,15 @@ server <- function(input, output) {
         }
     })
     
+    SummaryTableOrderString <- reactive({
+        if (input$COLOR_COLUMN == "Year") {
+            paste0("setorder(SummaryTable,-",input$COLOR_COLUMN,")")
+        } else {
+            paste0("setorder(SummaryTable,-N)")    
+        }
+        
+    })
+    
     output$RefreshDate <- renderText({
         paste0("Latest Incident Date: ",max(MurderData$Murder$Date,na.rm = TRUE),"")
         # if (!is.null(input$FILTER_COLUMN) && !is.null(input$FILTER_VALUE)) {
@@ -136,58 +162,13 @@ server <- function(input, output) {
     })
     
     observeEvent(input$RefreshData,{
-        print("Refreshing")
-        MurderRefresh()
+        MurderRefresh(MurderData$Murder)
     })
     
-    MurderData <- reactiveValues()
-    MurderData$Murder <- Murder
-    
-    MurderRefresh <- function() {
-        
-        lastRefreshDate <- max(Murder$Date,na.rm = TRUE)
-        
-        refreshString <- paste0("https://www.dallasopendata.com/Public-Safety/Police-Incidents/qv6i-rri7?$where=date1>'",
-                                lastRefreshDate,"'")
-        
-        PI <- read.socrata(refreshString)
-        setDT(PI)
-        NewMurder <- PI[grepl("Murder",offincident) | grepl("HOMICIDE",offincident) | grepl("Murder",ucr_offense) | 
-                         grepl("HOMICIDE",nibrs_crime_category) | grepl("Murder",nibrs_crime),]
-        
-        
-        if (nrow(NewMurder) > 0) {
-            NewMurder[,rowid := 1:.N]
-            NewMurder[,Date := as.Date(substr(date1,1,10))]
-            NewMurder[,MonthDate := as.Date(paste0(format(Date,"%Y-%m"),"-01"))]
-            NewMurder[,WeekNum := strftime(Date, format = "%V")]
-            NewMurder <- merge(NewMurder,NewMurder[,head(.SD, 1L),.SDcols = "Date",by = c("servyr","WeekNum")],by = c("servyr","WeekNum"))
-            setnames(NewMurder,old = c("Date.x","Date.y"),new = c("Date","WeekDate"))
-            
-            NewMurder[,LatLongStart := regexpr("(",geocoded_column,fixed = TRUE)[1] + 1,by = rowid]
-            NewMurder[,LatLongEnd := regexpr(")",geocoded_column,fixed = TRUE)[1] - 1,by = rowid]
-            NewMurder[,LatLong := substr(geocoded_column,start = LatLongStart,stop = LatLongEnd)]
-            NewMurder[,LatLongComma := regexpr(",",LatLong,fixed = TRUE)[1],by = rowid]
-            NewMurder[,Latitude := substr(LatLong,start = 1,stop = LatLongComma - 1)]
-            NewMurder[,Longitude := substr(LatLong,start = LatLongComma + 1,stop = 1000)]
-            NewMurder[,Longitude := as.numeric(Longitude)]
-            NewMurder[,Latitude := as.numeric(Latitude)]
-            NewMurder[,Year := as.factor(servyr)]
-            NewMurder$servyr <- NULL
-            NewMurder <- NewMurder[!is.na(Latitude) & !is.na(Longitude) & !is.na(Date) & !is.na(Year),]
-            
-            NewMurder[,c("LatLongStart","LatLongEnd","LatLong","LatLongComma") := NULL]
-            
-            setnames(NewMurder,old = c("watch","offincident","comprace","compsex","compage","compethnicity","status","victimcond"),
-                     c("Watch","Officer_Incident","Comp_Race","Comp_Sex","Comp_Age","Comp_Ethnicity","Status","Victim_Condition"))
-            MurderData$Murder <- rbindlist(list(MurderData$Murder,NewMurder),use.names = TRUE,fill = TRUE)
-        }
-        #Murder
-    }
-    
+
     output$SummaryTable <- renderTable({
         eval(parse(text = SummaryTableString()))
-        #setorder(SummaryTable,input$COLOR_COLUMN)
+        eval(parse(text = SummaryTableOrderString()))
         #SummaryTable
     })
     
@@ -219,7 +200,7 @@ server <- function(input, output) {
 
     output$LeafletMap <- renderLeaflet({
         
-        w <- input$FillOpacity
+        w <- 1
         fOp <- 1
         
         theMap <- leaflet(data = filtered_table()) %>% 
@@ -245,7 +226,17 @@ server <- function(input, output) {
                           pal = LegendPal, values = ~Watch,
                           title = "Legend",
                           opacity = 1)
-              
+            
+         } else if (input$COLOR_COLUMN == "Beat") {
+             
+             theMap <- theMap %>%  
+                 addCircleMarkers(lng = ~Longitude, lat = ~Latitude, weight = w, radius = input$MarkerSize,
+                                  fillOpacity = fOp,fillColor = ~pal()(Beat)) %>%
+                 addLegend(position = "bottomleft",
+                           pal = LegendPal, values = ~Beat,
+                           title = "Legend",
+                           opacity = 1)
+             
          } else if (input$COLOR_COLUMN == "Officer_Incident") {
              
              theMap <- theMap %>%  
@@ -312,6 +303,7 @@ server <- function(input, output) {
             addCircleMarkers(lng = ~Longitude, lat = ~Latitude, weight = 0, radius = input$MarkerSize,
                              fillOpacity = 0,
                              popup = ~paste0("Incident Date: ",Date,
+                                             "<BR> Beat: ",Beat,
                                              "<BR> Incident Address: ",incident_address,
                                              "<BR> Victim Sex: ",Comp_Sex,
                                              "<BR> Victim Age: ",Comp_Age,
@@ -322,7 +314,22 @@ server <- function(input, output) {
                                              "<BR> Signal: ",signal,
                                              "<BR> MO: ",mo))
         
+        if (input$NumClusters > 0) {
+            #KM <- kmeans(Murder[!is.na(Latitude) & !is.na(Longitude),.(Latitude,Longitude)],centers = 12)
+            #print(sizeVect)
+            
+            KM <- kmeans(filtered_table()[!is.na(Latitude) & !is.na(Longitude),.(Latitude,Longitude)],centers = input$NumClusters)
+            sizeVect <- round(scale(KM$size,center = 1)*10,digits = 0)
+            
+            # Plot the clusters
+            theMap <- theMap %>%  
+                addCircleMarkers(data = as.data.table(KM$centers),
+                    lng = ~Longitude, lat = ~Latitude, weight = 1, radius = sizeVect,
+                                 fillOpacity = 0.2,fillColor = "yellow")
+            
+        }
         
+        return(theMap)
     })
     
     
@@ -330,7 +337,8 @@ server <- function(input, output) {
         
         ggplot(filtered_table(),aes(x = Date)) + geom_histogram(bins = input$HIST_BINS,colour='red',size = 1) + 
                 stat_bin(bins = input$HIST_BINS,geom="text", colour="black", size=5.5,aes(label=..count..),
-                position = position_stack(vjust = 1.15))
+                position = position_stack(vjust = 1.15)) + 
+            theme(text = element_text(size=20),axis.text.x = element_text(size = 15)) 
         
     }, height = 200)
     
--- content/about.md
+++ content/about.md
@@ -1,6 +1,7 @@
 ---
 title: "About"
 date: '2020-12-02'
+output: html_document
 ---
 
 This is the personal website of Sean Conroy.
@@ -13,8 +14,3 @@ My interests are in applying statistics and machine learning to big hairy proble
 
 You can contact me at [seantconroy at gmail.com](seantconroy@gmail.com)
 
-
-
-
-
-The theme was forked from [@jrutheiser/hugo-lithium-theme](https://github.com/jrutheiser/hugo-lithium-theme) and modified by [Yihui Xie](https://github.com/yihui/hugo-lithium).
